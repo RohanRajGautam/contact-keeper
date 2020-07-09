63fe2ca1727627fabd6e8136db411bd48c1cf414
rohan <rohanrajgautam@gmail.com>
2020-07-09 19:25:07 +0545

    connect mongoDB to our app

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
+const connectDB = require("./config/db");
 
 const PORT = process.env.PORT || 5000;
+
+// CONNECT TO DATABASE
+connectDB();
 
 app.get("/", (req, res) =>
   res.json({ msg: "Welcome to the contact keeper API" })
